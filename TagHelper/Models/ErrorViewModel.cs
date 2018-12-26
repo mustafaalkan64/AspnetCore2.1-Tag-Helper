daea79632877700ac499f58732572037089a86cf
mustafa alkan <mustafaalkan64@gmail.com>
2018-12-26 14:39:20 +0300

    Tag Helper First Commit

---
+++ TagHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TagHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TagHelper/TagHelpers/MyCustomTagHelper.cs
@@ -0,0 +1,23 @@
+﻿
+namespace TagHelper.TagHelpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System.Text;
+
+    [HtmlTargetElement("custom-button-tag-helper")]
+    public class CustomTagHelper : TagHelper
+    {
+        [HtmlAttributeName("button-name")]
+        public string Name { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "CustumTagHelper";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var sb = new StringBuilder();
+            sb.AppendFormat("<button type='button' class='btn btn-primary'>{0}</button>", this.Name);
+
+            output.PreContent.SetHtmlContent(sb.ToString());
+        }
+    }
+}
